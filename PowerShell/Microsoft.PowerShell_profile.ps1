f902be6bac5184bd7ff4ddb7c0c9971a39cb5b4c
Eivind Teig <eivind.teig@gmail.com>
2024-12-21 18:06:32 +0100

    update repo.

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -30,5 +30,6 @@ function kr {
 Set-Item -Path Env:PIP_REQUIRE_VIRTUALENV -Value "true"
 Import-Module ZLocation
 Invoke-Expression (& { (zoxide init --cmd cd powershell | Out-String) })
+# Import-Module gsudoModule
 Invoke-Expression (&starship init powershell)
 Set-Alias ls -Value eza.exe
